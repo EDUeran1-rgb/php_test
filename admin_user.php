6cb1915c69494d8beb267095f7276ef2fc12e9ac
EDUeran1-rgb <EDUeran1@utb.karlskoga.se>
2026-01-27 15:17:33 +0100

    min egna

--- admin_user.php
+++ admin_user.php
@@ -9,6 +9,10 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <form action="admin_user.php" method="post">
+        <input type="text" name="user" id="user" placeholder="Användarnamn" required>
+        <input type="text" name="realname" id="realname" placeholder="Ditt namn" required>
+        <input type="password" name="pass" id="pass" placeholder="Lösenord" required>
+        <input type="submit" value="Skapa användare" name="btn">
 </body>
 </html>
